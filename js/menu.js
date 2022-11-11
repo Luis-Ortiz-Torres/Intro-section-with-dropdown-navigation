1a7591d73a8159e1a8fbcc5b8fd0f8691cdfe0ea
luis ortiz <luiscarortiz000118@gmail.com>
2022-11-11 12:07:46 -0500

    feat: funcionalidad del menu

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,26 @@ hamburguerIcon.addEventListener('click', ()=>{
 
     navOverlay.classList.toggle('nav__overlay--show')
 });
+
+navOverlay.addEventListener('click', (e) =>{
+    e.preventDefault();
+    const currentElement = e.target;
+
+    if (isActive(currentElement, 'nav__parent')){
+
+        const subMenu = currentElement.parentElement.children[1];
+        
+        if (window.innerWidth < 768) {
+            let height = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
+            subMenu.style.height = `${height}px`;
+        } else {
+            
+        }
+    }
+        
+    
+});
+
+function isActive(element, string){
+    return element.classList.value.includes(string);
+}
