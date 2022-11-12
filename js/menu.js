a569e5bb5c2856ca03a0e3d2a1384f7cf1f2cbbd
luis ortiz <luiscarortiz000118@gmail.com>
2022-11-12 15:01:11 -0500

    version final

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 const hamburguerIcon = document.querySelector('.nav__hamburguer');
 const navOverlay = document.querySelector('.nav__overlay');
+let currentDropdown = navOverlay;
 
 hamburguerIcon.addEventListener('click', ()=>{
     hamburguerIcon.classList.toggle('nav__hamburguer--open');
@@ -7,8 +8,10 @@ hamburguerIcon.addEventListener('click', ()=>{
     navOverlay.classList.toggle('nav__overlay--show')
 });
 
+
 navOverlay.addEventListener('click', (e) =>{
     e.preventDefault();
+
     const currentElement = e.target;
 
     if (isActive(currentElement, 'nav__parent')){
@@ -19,13 +22,32 @@ navOverlay.addEventListener('click', (e) =>{
             let height = (subMenu.clientHeight == 0) ? subMenu.scrollHeight : 0;
             subMenu.style.height = `${height}px`;
         } else {
-            
+
+            if (!isActive(subMenu, 'nav__inner--show')) {
+                closeDropdown(currentDropdown);
+                
+            }
+                subMenu.classList.toggle('nav__inner--show');
+                currentDropdown = subMenu;
         }
     }
-        
-    
 });
 
 function isActive(element, string){
     return element.classList.value.includes(string);
 }
+
+function closeDropdown(currentDropdown){
+    if (isActive(currentDropdown, 'nav__inner--show')) {
+        currentDropdown.classList.remove('nav__inner--show');
+    }
+}
+window.addEventListener('resize', ()=>{
+    closeDropdown(currentDropdown);
+    if (window.innerWidth > 768) {
+        const navInners = document.querySelectorAll('.nav__inner');
+        navInners.forEach(navInner =>{
+            navInner.style.height = '';
+        });
+    }
+});
